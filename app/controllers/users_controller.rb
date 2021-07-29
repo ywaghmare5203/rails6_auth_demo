181f6391df8a611871ddc6f1ec38275b052f618a
ywaghmare5203 <ywaghmare5203@gmail.com>
2021-07-29 09:23:32 +0530

    create registration from

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+	    @user = User.new(params[:user])
+	    if @user.save
+	      flash[:notice] = "You signed up successfully"
+	      flash[:color]= "valid"
+	    else
+	      flash[:notice] = "Form is invalid"
+	      flash[:color]= "invalid"
+	    end
+	    render "new"
+	 end
+
+	private
+	def form_params
+	  params.require(:user).permit(:username, :email, :firstname, :lastname, :phone, :hobby[], :gender, :status, :password_salt, :password_hash, :profile_photo)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :users
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
